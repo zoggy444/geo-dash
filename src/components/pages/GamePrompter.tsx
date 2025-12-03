9ceba0b751cebe24b7bfd24a95b5adcfbeb9d917
Nilrem <43614933+zoggy444@users.noreply.github.com>
2025-12-03 01:39:06 +0000

    ✨ F04: APP > GamePrompter > Edited 'active' behavior

--- src/components/pages/GamePrompter.tsx
+++ src/components/pages/GamePrompter.tsx
@@ -2,14 +2,28 @@ import { styled } from "@linaria/react";
 import type { GamePrompterProps } from "../../types/propsTypes";
 import Button from "../reusable-ui/Button";
 import Panel from "../reusable-ui/Panel";
+import { useEffect, useState } from "react";
 
 function GamePrompter({
   gameStage,
   toGuess,
   onStartGameClick,
 }: GamePrompterProps) {
+  const [active, setActive] = useState(true);
+  useEffect(() => {
+    if (gameStage == "round_in_progress") {
+      const timer = setTimeout(() => setActive(false), 1000);
+
+      return () => {
+        clearTimeout(timer);
+      };
+    }else if (!active){
+      setActive(true);
+    }
+  }, [gameStage, active]);
+
   return (
-    <GamePrompterStyled $active={gameStage !== "round_in_progress"}>
+    <GamePrompterStyled $active={active}>
       <Panel>
         {gameStage == "game_start" && <h2>Guess the place !</h2>}
         {gameStage == "round_in_progress" && (
